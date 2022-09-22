37b0439c8be6b7e539c0c7444dc1ff1b228d0611
woowsnu <woowsnu@gmail.com>
2022-09-22 19:47:30 +0900

    feat: cors 에러로 proxy 설정

--- src/apis/axios-instance.js
+++ src/apis/axios-instance.js
@@ -1,17 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:4000';
-// const token = localStorage.getItem('accessToken');
-const token =
-  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QyQHRlc3QuY29tIiwiaWF0IjoxNjYzODM5OTY3LCJleHAiOjE2NjM4NDM1NjcsInN1YiI6IjEwMiJ9.IwX670wJBh0TF9fZGDBJvgEOO6TUDDZIxb3PKu6QUlI';
-
+const token = localStorage.getItem('accessToken');
 axios.defaults.withCredentials = true;
 
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
   headers: {
-    // Authorization: 'Bearer ' + token
-    Authorization: token,
+    Authorization: 'Bearer ' + token,
     withCredentials: true,
   },
 });

2b03149368f269ad852bc50567682b1eefb264ac
dev.jun28 <hsj9360@gmail.com>
2022-09-23 01:21:32 +0900

    refactor(AccountDetail/index): change api calls
    계좌 api query를 이용, user_id로 다중 계좌 호출
    userId값 호출 후
    유저셋팅 api query를 이용, userId의 uuid로 유저셋팅 호출

--- client/src/pages/AccountDetail/AccountDetail.jsx
+++ client/src/pages/AccountDetail/AccountDetail.jsx
@@ -8,6 +8,7 @@ import { Api } from "../../api";
 import AccountDetailBreadCrumbs from "./bread-crumbs";
 import { Account } from "./components/Account";
 import { AccountUser } from "./components/AccountUser";
+import { UserSetting } from "./components/UserSetting";
 
 
 export const AccountDetail = () => {
@@ -15,16 +16,26 @@ export const AccountDetail = () => {
   const {id} = useParams();
 
   const fetcherAccount = async () => {
-    const {data: accountId} = await Api.getOneAccount.request({id});
+    const {data: accountId} = await Api.getAccountUser.request({
+      user_id: id
+    });
+
     const {data: userId} = await Api.getOneUser.request({userId: id});
+
+    const {data: userSetting} = await Api.getOneUserSetting.request({
+      uuid: userId.uuid
+    });
+
+    const userOneSetting = userSetting.find(item => userId.uuid === item.uuid);
 
     return {
       accountId,
       userId,
+      userOneSetting
     };
   };
 
-  const { data, isLoading } = useQuery(['accountDetail'], fetcherAccount, {
+  const {data, isLoading} = useQuery(['accountDetail'], fetcherAccount, {
     keepPreviousData: true,
     staleTime: 1000,
   });
@@ -36,10 +47,11 @@ export const AccountDetail = () => {
           <AccountBreadCrumb id={id}/>
           <Row gutter={10} justify="center">
             <Col span={25}>
-              <Account account={data.accountId}/>
+              <Account accounts={data.accountId}/>
             </Col>
             <Col span={25}>
               <AccountUser userAccount={data.userId}/>
+              <UserSetting userSetting={data.userOneSetting}/>
             </Col>
           </Row>
         </RootWrap>

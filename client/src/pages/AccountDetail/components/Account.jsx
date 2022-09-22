566402790b936c326d8cd1a6d785670db2dcd2b4
dev.jun28 <hsj9360@gmail.com>
2022-09-23 01:13:58 +0900

    style: add style

--- client/src/pages/AccountDetail/components/Account.jsx
+++ client/src/pages/AccountDetail/components/Account.jsx
@@ -1,64 +1,63 @@
 import { Card } from "antd";
-import React from "react";
 import styled from "styled-components";
 
 import brokers from "../../../data/brokers.json";
 import { boolRate, getDateFormat, getMaskingNumber, getOperatingStatus, getRate } from "../../../utils/account.util";
+import { CardWrap, DivWrap, TitleWrap } from "./style";
 
 export const Account = (props) => {
-  const { account } = props;
+  const {accounts} = props;
 
   return (
-    <CardWrap title="계좌 상세정보" bordered={true} bodyStyle={{width: '500px'}}>
-      <div>
-        <div>증권사</div>
-        <span>{brokers[account.broker_id]}</span>
-      </div>
-      <div>
-        <div>계좌번호</div>
-        {getMaskingNumber(account.number)}
-      </div>
-      <div>
-        <div>계좌명</div>
-        <span>{account.name}</span>
-      </div>
-      <div>
-        <div>운용상태</div>
-        <span>{getOperatingStatus(account.status)}</span>
-      </div>
-      <div>
-        <div>평가금액</div>
-        <span>{Number(account.assets).toLocaleString()}</span>
-      </div>
-      <div>
-        <div>입금금액</div>
-        <span>{Number(account.payments).toLocaleString()}</span>
-      </div>
-      <div>
-        <div>수익률</div>
-        <RateWrap isRate={boolRate(account.assets, account.payments)}>
-          {getRate(account.assets, account.payments)}
-        </RateWrap>
-      </div>
-      <div>
-        <div>계좌활성화</div>
-        <AccountActivationDivWrap isActive={account.is_active}>{account.is_active ? '활성화' : '비활성화'}</AccountActivationDivWrap>
-      </div>
-      <div>
-        <div>계좌개설일</div>
-        <span>{getDateFormat(account.created_at)}</span>
-      </div>
+    <CardWrap title="계좌 상세정보" bordered={true} bodyStyle={{width: '850px'}}>
+      {accounts.map((account) => {
+        return (
+          <Card.Grid key={account.uuid}>
+            <DivWrap>
+              <TitleWrap>증권사</TitleWrap>
+              <span>{brokers[account.broker_id]}</span>
+            </DivWrap>
+            <DivWrap>
+              <TitleWrap>계좌번호</TitleWrap>
+              {getMaskingNumber(account.number)}
+            </DivWrap>
+            <DivWrap>
+              <TitleWrap>계좌명</TitleWrap>
+              <span>{account.name}</span>
+            </DivWrap>
+            <DivWrap>
+              <TitleWrap>운용상태</TitleWrap>
+              <span>{getOperatingStatus(account.status)}</span>
+            </DivWrap>
+            <DivWrap>
+              <TitleWrap>평가금액</TitleWrap>
+              <span>{Number(account.assets).toLocaleString()}</span>
+            </DivWrap>
+            <DivWrap>
+              <TitleWrap>입금금액</TitleWrap>
+              <span>{Number(account.payments).toLocaleString()}</span>
+            </DivWrap>
+            <DivWrap>
+              <TitleWrap>수익률</TitleWrap>
+              <RateWrap isRate={boolRate(account.assets, account.payments)}>
+                {getRate(account.assets, account.payments)}
+              </RateWrap>
+            </DivWrap>
+            <DivWrap>
+              <TitleWrap>계좌활성화</TitleWrap>
+              <AccountActivationDivWrap isActive={account.is_active}>{account.is_active ? '활성화' : '비활성화'}</AccountActivationDivWrap>
+            </DivWrap>
+            <DivWrap>
+              <TitleWrap>계좌개설일</TitleWrap>
+              <span>{getDateFormat(account.created_at)}</span>
+            </DivWrap>
+          </Card.Grid>
+        );
+      })}
     </CardWrap>
   );
 };
 
-const CardWrap = styled(Card)`
-  height: 100%;
-
-  .ant-card-extra {
-    padding: 0;
-  }
-`;
 const AccountActivationDivWrap = styled.div`
   color: ${(props) => props.isActive ? 'red' : 'blue'}
 `;
--- client/src/pages/AccountDetail/components/AccountUser.jsx
+++ client/src/pages/AccountDetail/components/AccountUser.jsx
@@ -1,57 +1,48 @@
-import { Avatar, Card } from "antd";
-import React from "react";
+import { Avatar } from "antd";
 import styled from "styled-components";
 
-import { getDateFormat } from "../../../utils/account.util";
+import { getDateFormat, getMaskingPhoneNumber } from "../../../utils/account.util";
+import { CardWrap, DivWrap, TitleWrap } from "./style";
 
 export const AccountUser = (props) => {
-  const { userAccount } = props;
+  const {userAccount} = props;
 
   return (
     <CardWrap title={userAccount.name} bordered={true} extra={
-      <AvatarWrap src={userAccount.photo} size={36}/>} bodyStyle={{width: '500px'}}>
-      <div>
-        <div>주소</div>
+      <AvatarWrap src={userAccount.photo} size={36}/>} bodyStyle={{width: '400px'}}>
+      <DivWrap>
+        <TitleWrap>주소</TitleWrap>
         <span>{`${userAccount.address} ${userAccount.detail_address}`}</span>
-      </div>
-      <div>
-        <div>생일</div>
+      </DivWrap>
+      <DivWrap>
+        <TitleWrap>생일</TitleWrap>
         <span>{getDateFormat(userAccount.birth_date)}</span>
-      </div>
-      <div>
-        <div>아이디 생성일</div>
+      </DivWrap>
+      <DivWrap>
+        <TitleWrap>아이디 생성일</TitleWrap>
         <span>{getDateFormat(userAccount.created_at)}</span>
-      </div>
-      <div>
-        <div>이메일</div>
+      </DivWrap>
+      <DivWrap>
+        <TitleWrap>이메일</TitleWrap>
         <span>{userAccount.email}</span>
-      </div>
-      <div>
-        <div>마지막 접속일</div>
-        <span>{userAccount.last_login}</span>
-      </div>
-      <div>
-        <div>핸드폰 번호</div>
-        <span>{userAccount.phone_number}</span>
-      </div>
-      <div>
-        <div>마지막 수정일</div>
+      </DivWrap>
+      <DivWrap>
+        <TitleWrap>마지막 접속일</TitleWrap>
+        <span>{getDateFormat(userAccount.last_login)}</span>
+      </DivWrap>
+      <DivWrap>
+        <TitleWrap>핸드폰 번호</TitleWrap>
+        <span>{getMaskingPhoneNumber(userAccount.phone_number)}</span>
+      </DivWrap>
+      <DivWrap>
+        <TitleWrap>마지막 수정일</TitleWrap>
         <span>{getDateFormat(userAccount.updated_at)}</span>
-      </div>
+      </DivWrap>
     </CardWrap>
   );
 };
 
-
-const CardWrap = styled(Card)`
-  height: 100%;
-
-  .ant-card-extra {
-    padding: 0;
-  }
-`;
-
-const AvatarWrap = styled(Avatar)`
+export const AvatarWrap = styled(Avatar)`
   margin-left: 10px;
   padding: 0;
 `;

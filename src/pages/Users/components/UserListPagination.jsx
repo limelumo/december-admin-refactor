ca5417c2dc0c8806b3eef4afba28ea22d01e8913
Lihu Kim <lh.ines.kim@gmail.com>
2022-09-22 18:01:49 +0900

    feat: UserListPagination 경로 변경 및 스타일링 적용
    폴더명 변경(UserList > Users), antd 적용

---
+++ src/pages/Users/components/UserListPagination.jsx
@@ -0,0 +1,32 @@
+import 'antd/dist/antd.css';
+
+import { Pagination } from 'antd';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+
+import { currentPageState, dataTotalCountState } from '../../../utils/userListStore';
+
+const UserListPagination = () => {
+  const dataTotalCount = useRecoilValue(dataTotalCountState);
+
+  const [currentPage, setCurrentPage] = useRecoilState(currentPageState);
+
+  const handlePageButton = (page) => setCurrentPage(page);
+
+  return (
+    <PaginationContainer>
+      <Pagination current={currentPage} total={dataTotalCount} onChange={handlePageButton} />
+    </PaginationContainer>
+  );
+};
+
+const PaginationContainer = styled.div`
+  margin-top: 3em;
+  text-align: center;
+
+  .ant-pagination-options {
+    display: none;
+  }
+`;
+
+export default UserListPagination;

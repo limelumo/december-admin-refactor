3d827e4661e4c7d1795ee59cc35b1541c70d731c
ycs1m1yk <satoly4@gmail.com>
2022-09-24 03:00:18 +0900

    docs: 완료한 todo 주석 삭제

--- src/pages/accounts/accounts.jsx
+++ src/pages/accounts/accounts.jsx
@@ -8,14 +8,9 @@ import { COLUMNS } from './accounts.constants';
 import { formatAccountData } from './accounts.utils';
 
 /**
- * - 표기되어야 하는 정보
-    - @todo 특정 계좌를 누르면 계좌의 상세화면으로 이동합니다. ??? 계좌 id를 어떻게 해야할까 ???
-    - @todo 계좌 수정 기능
-    - @todo 목록에서는 브로커명, 계좌 활성화 여부, 계좌 상태를 필터링 할 수 있어야 합니다.
-    - @todo 리스트 페이지에서는 검색이 가능해야 합니다.
-        - `json-server` 의 Full-text Search API 를 사용하여 구현합니다.
-    - @todo 페이지네이션이 되어야 합니다.
-        - `json-server` 의 Paginate API 를 사용하여 구현합니다.
+  - @todo 계좌 수정 기능
+  - @todo 리스트 페이지에서는 검색이 가능해야 합니다.
+      - `json-server` 의 Full-text Search API 를 사용하여 구현합니다.
  */
 const Accounts = () => {
   const [accounts, setAccounts] = useState();

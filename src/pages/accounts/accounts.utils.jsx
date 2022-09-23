9a43536c4a825103421213ad16524cde72846c0d
ycs1m1yk <satoly4@gmail.com>
2022-09-23 20:55:52 +0900

    feat: 계좌목록 페이지 추가
    - andt Table 사용
    - react-query 데이터 fetch
    - data formatting
    - 유저 상세 링크
    - 계좌 상세 링크

---
+++ src/pages/accounts/accounts.utils.jsx
@@ -0,0 +1,40 @@
+import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
+import { accountStatus, brokerFormat, brokers } from '../../data';
+
+const maskNumber = (number) => {
+  const numberArr = [...String(number)];
+  for (let [left, right] = [0, numberArr.length - 1]; left <= 1; left += 1, right -= 1) {
+    numberArr[left] = '*';
+    numberArr[right] = '*';
+  }
+  return numberArr.join('');
+};
+
+const formatAccountNumber = (broker_id, number) => {
+  const numberArr = [...maskNumber(number)];
+  const matches = brokerFormat[broker_id]?.matchAll('-');
+  if (matches) {
+    [...matches].forEach(({ index }, i) => (numberArr[index - i] = '-' + numberArr[index - i]));
+  }
+
+  return numberArr.join('');
+};
+
+const formatPrice = (price) => Math.trunc(Number(price)).toLocaleString();
+
+const getKeyByValue = (object, value) => {
+  return Object.keys(object).find((key) => object[key] === value);
+};
+
+export const formatAccountData = ({ broker_id, number, name, assets, payments, created_at, is_active, status }) => ({
+  broker_name: brokers[broker_id],
+  number: <Link to={`/account/${'id를 어떻게하지'}}`}>{formatAccountNumber(broker_id, number)}</Link>,
+  name,
+  assets: formatPrice(assets),
+  payments: formatPrice(payments),
+  created_at: created_at?.slice(0, 10),
+  is_active: is_active ? <CheckOutlined style={{ color: 'green' }} /> : <CloseOutlined style={{ color: 'red' }} />,
+  status: getKeyByValue(accountStatus, status),
+});

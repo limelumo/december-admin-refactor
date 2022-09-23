5c560a2bcabe7337864f79d4b26ba68a21e13d50
ycs1m1yk <satoly4@gmail.com>
2022-09-24 00:12:12 +0900

    fix: 라우팅 수정
    - /users가 Users 컴포넌트 렌더링
    - antd css import 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import 'antd/dist/antd.css';
-
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -8,7 +6,7 @@ import Accounts from './pages/accounts/accounts';
 import Login from './pages/Login/Login';
 import Main from './pages/Main';
 import UserDetail from './pages/userdetail/UserDetail';
-import UserList from './pages/Users/components/UserList';
+import Users from './pages/Users/Users';
 
 const queryClient = new QueryClient();
 
@@ -21,8 +19,8 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/accounts" element={<Accounts />} />
           <Route path="/account/:id" element={<AccountDetail />} />
-          <Route path="/users" element={<UserList />} />
-          <Route path="/users/:user_id" element={<UserDetail />}></Route>
+          <Route path="/users" element={<Users />} />
+          <Route path="/users/:user_id" element={<UserDetail />} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
       </BrowserRouter>

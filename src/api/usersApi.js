43d0ecc7cb47d1e0708b2c505807d631ddb3b750
Lihu Kim <lh.ines.kim@gmail.com>
2022-09-24 18:52:14 +0900

    feat: usersApi 수정
    params 정리

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,15 +1,17 @@
 import { axiosInstance } from './axios-instance';
 
 const usersApi = {
-  getUsersData: async ({ params }) => await axiosInstance.get('/users', { params }),
+  getUsersData: async (params={}) => await axiosInstance.get('/users', { params }),
 
   getUserSettingData: async () => await axiosInstance.get('/userSetting'),
 
-  getSearchData: async ({ params }) => await axiosInstance.get('/users', { params }),
+  getAccountsData: async () => await axiosInstance.get('/accounts'),
+
+  getSearchData: async (params={}) => await axiosInstance.get('/users', { params }),
 
   addNewUserData: async (config) => await axios.post('/users', config),
 
-  updateUserData: async ({ params }) => await axios.patch('/users', { params }),
+  updateUserData: async (params={}) => await axios.patch('/users', { params }),
 
   removeUser: async (targetId) => await axiosInstance.delete(`/users/${targetId}`),
 };

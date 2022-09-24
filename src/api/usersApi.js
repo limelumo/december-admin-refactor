9ee17a78f9333432536f6061e15742f27c9aa4fc
Lihu Kim <lh.ines.kim@gmail.com>
2022-09-25 00:42:36 +0900

    feat: usersApi 수정

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,17 +1,17 @@
 import { axiosInstance } from './axios-instance';
 
 const usersApi = {
-  getUsersData: async (params={}) => await axiosInstance.get('/users', { params }),
+  getUsersData: async (params = {}) => await axiosInstance.get('/users', { params }),
 
   getUserSettingData: async () => await axiosInstance.get('/userSetting'),
 
   getAccountsData: async () => await axiosInstance.get('/accounts'),
 
-  getSearchData: async (params={}) => await axiosInstance.get('/users', { params }),
+  getSearchData: async (params = {}) => await axiosInstance.get('/users', { params }),
 
-  addNewUserData: async (config) => await axios.post('/users', config),
+  addNewUserData: async (config) => await axiosInstance.post('/users', config),
 
-  updateUserData: async (params={}) => await axios.patch('/users', { params }),
+  updateUserData: async (targetId, config) => await axiosInstance.patch(`/users/${targetId}`, { name: config }),
 
   removeUser: async (targetId) => await axiosInstance.delete(`/users/${targetId}`),
 };

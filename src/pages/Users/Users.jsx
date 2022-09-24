c724ca30916cffa979c4bac4826625bb97aa1c9f
Lihu Kim <lh.ines.kim@gmail.com>
2022-09-25 00:52:41 +0900

    feat: users api 호출 로직 정리

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,14 +1,14 @@
 import usersApi from 'api/usersApi';
 import { useQuery } from 'react-query';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
-import { formatUsersData } from 'utils/formatUsersData';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import {
   accountsState,
   currentPageState,
   dataTotalCountState,
   usersDataState,
   userSettingDataState,
-} from 'utils/userListStore';
+} from 'store/userList';
+import { formatUsersData } from 'utils/formatUsersData';
 
 import UserList from './components/UserList';
 import UserListPagination from './components/UserListPagination';
@@ -16,12 +16,13 @@ import UserMenu from './components/UserMenu';
 
 const Users = () => {
   const currentPage = useRecoilValue(currentPageState);
-
-  const [usersData, setUsersData] = useRecoilState(usersDataState);
-  const [userSettingData, setUserSettingData] = useRecoilState(userSettingDataState);
-  const [accountsData, setAccountsData] = useRecoilState(accountsState);
+  const userSettingData = useRecoilValue(userSettingDataState);
+  const accountsData = useRecoilValue(accountsState);
 
   const setDataTotalCount = useSetRecoilState(dataTotalCountState);
+  const setUserSettingData = useSetRecoilState(userSettingDataState);
+  const setAccountsData = useSetRecoilState(accountsState);
+  const setUsersData = useSetRecoilState(usersDataState);
 
   const { data: accounts } = useQuery(['accounts'], usersApi.getAccountsData, {
     refetchOnWindowFocus: false,
@@ -37,39 +38,33 @@ const Users = () => {
     onSuccess: (res) => setUserSettingData(res.data),
   });
 
-  // params를 비워두고 재활용
+  useQuery(['users', currentPage], () => usersApi.getUsersData({ _limit: 10, _page: currentPage }), {
+    refetchOnWindowFocus: false,
+    keepPreviousData: true,
+    // eslint-disable-next-line no-implicit-coercion
+    enabled: !!userSetting,
+    onSuccess: (res) => {
+      setDataTotalCount(res.headers['x-total-count']);
 
- useQuery(
-    ['users', currentPage],
-    () => usersApi.getUsersData({ _limit: 10, _page: currentPage }),
-    {
-      refetchOnWindowFocus: false,
-      keepPreviousData: true,
-      // eslint-disable-next-line no-implicit-coercion
-      enabled: !!userSetting,
-      onSuccess: (res) => {
-        setDataTotalCount(res.headers['x-total-count']);
+      const accountsUserId = accountsData.map((el) => el.user_id);
+      const accountsMatched = accountsUserId.reduce((accu, curr) => {
+        accu[curr] = (accu[curr] || 0) + 1;
+        return accu;
+      }, {});
 
-        const accountsUserId = accountsData.map((el) => el.user_id);
-        const accountsMatched = accountsUserId.reduce((accu, curr) => {
-          accu[curr] = (accu[curr] || 0) + 1;
-          return accu;
-        }, {});
-
-        setUsersData(
-          res.data.map((user) => {
-            return {
-              is_active: userSettingData?.find((el) => el.uuid === user.uuid)?.is_active,
-              is_staff: userSettingData?.find((el) => el.uuid === user.uuid)?.is_staff,
-              allow_marketing_push: userSettingData?.find((el) => el.uuid === user.uuid)?.allow_marketing_push,
-              account_count: accountsMatched[user.id],
-              ...formatUsersData(user),
-            };
-          })
-        );
-      },
-    }
-  );
+      setUsersData(
+        res.data?.map((user) => {
+          return {
+            is_active: userSettingData?.find((el) => el.uuid === user.uuid)?.is_active.toString(),
+            is_staff: userSettingData?.find((el) => el.uuid === user.uuid)?.is_staff.toString(),
+            allow_marketing_push: userSettingData?.find((el) => el.uuid === user.uuid)?.allow_marketing_push.toString(),
+            account_count: accountsMatched[user.id],
+            ...formatUsersData(user),
+          };
+        })
+      );
+    },
+  });
 
   return (
     <>

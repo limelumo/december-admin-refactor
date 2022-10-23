134f890c7fa42d0646fa07298dbb9e988541f1e6
Lihu Kim <lh.ines.kim@gmail.com>
2022-10-23 23:01:14 +0900

    delete: 불필요한 내용 삭제
    antd Table을 적용하며 필요없어진 컴포넌트, store값 삭제

--- src/store/userList.js
+++ src/store/userList.js
@@ -1,7 +1,5 @@
 import { atom } from 'recoil';
 
-const randomNum = Math.random() + Date.now().toString();
-
 export const usersDataState = atom({
   key: 'usersDataState',
   default: [],
@@ -18,32 +16,8 @@ export const accountsState = atom({
 });
 
 // Pagination
-export const currentPageState = atom({
-  key: 'currentPageState',
-  default: 1,
-});
-
 export const dataTotalCountState = atom({
   key: 'dataTotalCountState',
   default: 0,
 });
 
-// Modal
-export const userInfoState = atom({
-  key: 'userInfoState',
-  default: {
-    id: null,
-    uuid: randomNum,
-    name: '',
-    account_count: '',
-    email: '',
-    gender_origin: 0,
-    birth_date: '',
-    phone_number: '',
-    last_login: '',
-    allow_marketing_push: true,
-    is_active: true,
-    is_staff: true,
-    created_at: '',
-  },
-});
